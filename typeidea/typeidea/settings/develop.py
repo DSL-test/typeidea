e3caf60b2e82a53ad487eaa0722e30248f2c6705
DSL <596553705@qq.com>
2020-04-13 21:15:39 +0800

    初始化提交

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,15 @@
+from .base import import *  # NOQA
+
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'typeidea',
+        'USER': 'root',
+        'PASSWORD': 'Qq83681500!',
+        'HOST': '39.105.80.79',
+        'POST': '3306',
+    }
+}
